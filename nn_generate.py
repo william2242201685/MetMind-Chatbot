78e69866e38b05a979a219edcd1b251053dce195
william2242201685 <57372703+william2242201685@users.noreply.github.com>
2021-05-10 20:39:20 +0100

    Update nn_generate.py

--- nn_generate.py
+++ nn_generate.py
@@ -2,6 +2,7 @@
 # 1: Tech With Tim (2019) Python Chat Bot Tutorial - Chatbot with Deep Learning (Part 3) Available At: https://www.youtube.com/watch?v=PzzHOvpqDYs&ab_channel=TechWithTim (Accessed: 16/10/2020)
 # 2: ugik (2017) Tensorflow chat-bot model.ipynb Available At:  https://github.com/ugik/notebooks/blob/master/Tensorflow%20chat-bot%20model.ipynb (Accessed: 2/02/2021)
 
+
 import tflearn
 import tensorflow as tf
 import numpy as np
@@ -20,36 +21,31 @@ from nltk.stem.lancaster import LancasterStemmer
 
 stemmer = LancasterStemmer()
 
-# create our training data
+
 training = []
 output = []
-# create an empty array for our output
 output_empty = [0] * len(json_build.classes)
 
-# training set, bag of words for each sentence
+# This section is designed to transform each sentence into a BOW[Bag of Words]
 for doc in json_build.documents:
     bag_of_words = []
-    # list of tokenized words for the pattern
     pattern_words = doc[0]
-    # stem each word
     pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
-    # bag_of_words array
     for w in json_build.words:
         if w in pattern_words:
             bag_of_words.append(1)
         else:
             bag_of_words.append(0)
-    # output is a '0' for each tag and '1' for current tag
     output_row = list(output_empty)
     output_row[json_build.classes.index(doc[1])] = 1
 
     training.append([bag_of_words, output_row])
 
-# shuffle our features and turn into np.array
+
 random.shuffle(training)
 training = np.array(training)
 
-# create train and test lists
+# This will create our training and testing data as lists
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
 
@@ -70,6 +66,7 @@ except ValueError as nnbuilderror:
     print("Error creating the neural network model: ", nnbuilderror)
     sys.exit('Closing Program')
 
+#If path is not in directory try to create folder ./tflearnModel
 if not os.path.isdir('./tflearnModel'):
     try:
         os.mkdir("./tflearnModel")
@@ -79,7 +76,7 @@ if not os.path.isdir('./tflearnModel'):
     else:
         print("Successfully created the directory")
 
-# Define model and setup tensorboard
+# This section is designed to created our neural networking model
 try:
     model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
     # Start training (apply gradient descent algorithm)
@@ -89,6 +86,8 @@ except IndexError as nnfiterror:
     print("Error fitting the neural network model: ", nnfiterror)
     sys.exit('Closing Program')
 
+
+#Try to pickle words/classes and training data as to convert and save  words/classes and training data into bytes.
 try:
     pickle.dump({'words': json_build.words, 'classes': json_build.classes, 'train_x': train_x, 'train_y': train_y},
                 open("./tflearnModel/model.tflearn", "wb"))
